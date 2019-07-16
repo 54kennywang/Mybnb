0f8041daaf7dca529224f9fb57e38b5397a93054
Eric Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-16 18:42:31 -0400

    updated report

--- src/test/Report.java
+++ src/test/Report.java
@@ -1,12 +1,16 @@
 package test;
 
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.List;
 
 public class Report {
 
-    public static void numOfBookingsByCity(List<LocalDate> dateRange, String city) {
-
+    public static int numOfBookingsByCity(LocalDate fromDate, LocalDate toDate, String city) {
+        // natural join address, listing, and rented
+        String query = "select count(status) from listing, address, rented where city = " + city + " and status = " + 0 + " and ";
+        ResultSet resultSet = Database.queryRead(query);
+        return 0;
     }
 
     public static void numOfBookingByZipCode(List<LocalDate> dateRange, String zipCode){
