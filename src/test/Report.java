4999fc6d78e0197afd6d7f250a9fe94631f3efe6
Eric Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-16 17:05:36 -0400

    updated report

--- src/test/Report.java
+++ src/test/Report.java
@@ -1,4 +1,15 @@
 package test;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Report {
+
+    public static void numOfBookingsByCity(List<LocalDate> dateRange, String city) {
+
+    }
+
+    public static void numOfBookingByZipCode(List<LocalDate> dateRange, String zipCode){
+
+    }
 }
