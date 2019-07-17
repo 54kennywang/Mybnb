107ab913ef054df38965ff0596393fbc3a542f23
Eric Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-17 14:13:33 -0400

    finished a query

--- src/test/Report.java
+++ src/test/Report.java
@@ -3,17 +3,32 @@ package test;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.List;
+import java.sql.SQLException;
 
 public class Report {
 
-    public static int numOfBookingsByCity(LocalDate fromDate, LocalDate toDate, String city) {
-        // natural join address, listing, and rented
-        String query = "select count(status) from listing, address, rented where city = " + city + " and status = " + 0 + " and ";
+    public static int numOfBookingsByCity(LocalDate fromDate, LocalDate toDate, String city) throws SQLException {
+        //remove "-" from date
+        String fDate = fromDate.toString().replaceAll("-","");
+        String tDate = toDate.toString().replaceAll("-","");
+        // natural join address, and rented
+        String query = "select count(status) from address join rented on address.id = rented.l_id " +
+                "where city = '" + city + "' and status = " + 0 + " and fromDate >= " + fDate + " and toDate <= " + tDate + ";";
         ResultSet resultSet = Database.queryRead(query);
-        return 0;
+        resultSet.next();
+        return resultSet.getInt("count(status)");
     }
 
     public static void numOfBookingByZipCode(List<LocalDate> dateRange, String zipCode){
 
+    }
+
+    public static void main(String args[]) throws SQLException {
+        if (Database.connect()){
+            LocalDate fromDate = LocalDate.parse("2020-01-01");
+            LocalDate toDate = LocalDate.parse("2021-01-01");
+            System.out.println(numOfBookingsByCity(fromDate, toDate, "London"));
+        }
+        Database.disconnect();
     }
 }
